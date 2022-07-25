65a646daca734668ea95fbe9d55cf011c345e5c5
hodob <hodob76@gmail.com>
2022-07-25 12:47:13 +0900

    Final

--- app.js
+++ app.js
@@ -1,42 +1,19 @@
 const numbers = document.querySelectorAll('.num');
 const submit = document.querySelector('.submit');
+const page = document.getElementById('page');
 let numSelected = 0;
-let actived = false;
-console.log(numbers[1].textContent)
-
 for (let i = 0; i < numbers.length; i++) {
-  // numbers[i].addEventListener('click', () => numSelected = i + 1);
-  if (i + 1 !== numbers[i].textContent) {
-    console.log("sfafd")
 
-  }
-}
-
-
-
-
-//mouser click
-
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener('click', () => numbers[i].classList.toggle('num_click'));
-
-  //numbers
-
+  ////numbers////
+  //mouser click
+  numbers[i].addEventListener('click', () => numberSelected(i));
   //mouse over
   numbers[i].addEventListener('mouseenter', () => numbers[i].classList.add('num_hover'));
-
   //mouser leave
   numbers[i].addEventListener('mouseleave', () => numbers[i].classList.remove('num_hover'));
 }
 
-
-
-
-
-
-
-
-//sumbit
+////sumbit////
 //submit over
 submit.addEventListener('mouseenter', () => submit.classList.add('submit_hover'));
 
@@ -44,16 +21,26 @@ submit.addEventListener('mouseenter', () => submit.classList.add('submit_hover')
 submit.addEventListener('mouseleave', () => submit.classList.remove('submit_hover'));
 
 //submit clicked
-submit.addEventListener('clicked', () => document.querySelector('page').innerHTML = `
+submit.addEventListener('click', () => page.innerHTML = `
 <div class="container_2">
-      <div class="finalImage"></div>
-      <div class="selectNumResult">You selected numSelected out of 5</div>
-      <div class="titleText_2">
-        Thank You!
-      </div>
-      <div class="bodyText_2">
-      <div>We appreciate you taking the time to give a rating.</div>
-      <div>if you ever need mor support, don't hesitate to get in touch!</div>
-      </div>
+<div class="finalImage"></div>
+<div class="selectNumResult">You selected  ${numSelected} out of 5</div>
+<div class="titleText">
+    Thank You!
+</div>
+<div class="bodyText">
+  <div>We appreciate you taking the time to give a rating.</div>
+  <div>if you ever need mor support, don't hesitate to get in touch!</div>
+  </div>
+</div>
 `);
 
+////function////
+function numberSelected(i) {
+  for (let j = 0; j < numbers.length; j++) {
+    if (i == j) {
+      numbers[i].classList.add('num_click');
+      numSelected = i + 1;
+    } else { numbers[j].classList.remove('num_click'); }
+  }
+}
